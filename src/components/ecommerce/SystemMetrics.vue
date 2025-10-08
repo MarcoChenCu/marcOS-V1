cf468fb4c217c06760b22612fd5b1bdd8bd0728c
MarcoChenCu <alfredochencu@gmail.com>
2025-10-08 01:00:35 -0600

    Configurado metricas dashboard

--- src/components/ecommerce/SystemMetrics.vue
+++ src/components/ecommerce/SystemMetrics.vue
@@ -1,27 +1,17 @@
-<script setup lang="ts">
-import { defineProps, computed } from "vue";
-
-const props = defineProps({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
-  icon: { type: Object, required: true }, // componente de icono
-  value: { type: Number, required: true }, // porcentaje o métrica numérica
-});
-
-// Colores dinámicos según valor (opcional)
-const valueColorClass = computed(() => {
-  if (props.value >= 80) return "bg-danger-50 text-danger-600 dark:bg-danger-500/15 dark:text-danger-500";
-  if (props.value >= 50) return "bg-warning-50 text-warning-600 dark:bg-warning-500/15 dark:text-warning-500";
-  return "bg-success-50 text-success-600 dark:bg-success-500/15 dark:text-success-500";
-});
+<script setup>
+defineProps({
+  title: String,
+  description: String,
+  value: Number,
+})
 </script>
 
 <template>
   <div class="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-6">
     <div class="flex items-center justify-between mt-2">
       <div class="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-xl dark:bg-gray-800">
-        <!-- Ícono pasado como prop -->
-        <component :is="icon" class="w-6 h-6 fill-gray-800 dark:fill-white/90" />
+        <!-- Ícono -->
+        <slot name="icon"/>
       </div>
 
       <span
--- src/views/Ecommerce.vue
+++ src/views/Ecommerce.vue
@@ -2,13 +2,38 @@
   <admin-layout>
     <div class="grid grid-cols-12 gap-4 md:gap-6">
       <div class="col-span-12 space-y-6 xl:col-span-7">
-        <ecommerce-metrics />
-        <SystemMetricCard
-          title="Procesador"
-          description="4 x Intel(R) Xeon(R) CPU E3-1225 v5 @ 3.30GHz"
-          :icon="CPUIcon"
-          :value="cpuUsage"
-        />
+        <!--Metricas del sistema-->
+        <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-6">          
+            <SystemMetrics
+            title="Procesador"
+            description="Intel Xeon E3-1225 v5"
+            :value="50"
+            >
+            <template #icon>
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-cpu-icon lucide-cpu"><path d="M12 20v2"/><path d="M12 2v2"/><path d="M17 20v2"/><path d="M17 2v2"/><path d="M2 12h2"/><path d="M2 17h2"/><path d="M2 7h2"/><path d="M20 12h2"/><path d="M20 17h2"/><path d="M20 7h2"/><path d="M7 20v2"/><path d="M7 2v2"/><rect x="4" y="4" width="16" height="16" rx="2"/><rect x="8" y="8" width="8" height="8" rx="1"/></svg>
+            </template>
+          </SystemMetrics>
+
+          <SystemMetrics
+            title="RAM"
+            description="4GB"
+            :value="50"
+          >
+            <template #icon>
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-cpu-icon lucide-cpu"><path d="M12 20v2"/><path d="M12 2v2"/><path d="M17 20v2"/><path d="M17 2v2"/><path d="M2 12h2"/><path d="M2 17h2"/><path d="M2 7h2"/><path d="M20 12h2"/><path d="M20 17h2"/><path d="M20 7h2"/><path d="M7 20v2"/><path d="M7 2v2"/><rect x="4" y="4" width="16" height="16" rx="2"/><rect x="8" y="8" width="8" height="8" rx="1"/></svg>
+            </template>
+          </SystemMetrics>
+
+          <SystemMetrics
+            title="Almacenamiento"
+            description="128GB"
+            :value="50"
+          >
+            <template #icon>
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-cpu-icon lucide-cpu"><path d="M12 20v2"/><path d="M12 2v2"/><path d="M17 20v2"/><path d="M17 2v2"/><path d="M2 12h2"/><path d="M2 17h2"/><path d="M2 7h2"/><path d="M20 12h2"/><path d="M20 17h2"/><path d="M20 7h2"/><path d="M7 20v2"/><path d="M7 2v2"/><rect x="4" y="4" width="16" height="16" rx="2"/><rect x="8" y="8" width="8" height="8" rx="1"/></svg>
+            </template>
+          </SystemMetrics>
+        </div>
       </div>
       <div class="col-span-12 xl:col-span-5">
         <monthly-sale />
@@ -32,16 +57,18 @@
 <script>
 import AdminLayout from '../components/layout/AdminLayout.vue'
 import EcommerceMetrics from '../components/ecommerce/EcommerceMetrics.vue'
+import SystemMetrics from '@/components/ecommerce/SystemMetrics.vue'
 import MonthlyTarget from '../components/ecommerce/MonthlySale.vue'
 import MonthlySale from '../components/ecommerce/MonthlyTarget.vue'
 import CustomerDemographic from '../components/ecommerce/CustomerDemographic.vue'
 import StatisticsChart from '../components/ecommerce/StatisticsChart.vue'
 import RecentOrders from '../components/ecommerce/RecentOrders.vue'
-import { CPUIcon } from "@/icons"
+
 export default {
   components: {
     AdminLayout,
     EcommerceMetrics,
+    SystemMetrics,
     MonthlyTarget,
     MonthlySale,
     CustomerDemographic,
